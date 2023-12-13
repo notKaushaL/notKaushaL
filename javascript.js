c0f4801228cf062bb3681280911a1f29020a7232
notKaushaL <22airad052@ldce.ac.in>
2023-12-13 21:56:44 +0530

    Jai Shree Ram

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("registrationForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Registration successful!"); // You can replace this with your registration logic
+});
